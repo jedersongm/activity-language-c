9912f41b779d058f8854a90be6688597630f424b
Jéderson Gonçalves Martins <jedersongmoura@gmail.com>
2018-03-11 04:53:58 -0300

    Question6: programa da lê 10 numeros distintos

---
+++ question6.c
@@ -0,0 +1,41 @@
+/*
+6 -Faça um programa para ler 10 números DIFERENTES a serem armazenados em um vetor.
+Os dados deverão ser armazenados no vetor na ordem que forem sendo lidos, sendo que
+caso o usuário digite um número que já foi digitado anteriormente, o programa devera
+pedir para ele digitar outro número. Note que cada valor digitado pelo usuário deve ser
+pesquisado no vetor, verificando se ele existe entre os números que já foram fornecidos.
+*/
+
+#include <stdio.h>
+
+int isExist(int vector[], int number){
+
+  for(int i = 0; i < 10; i++ )
+    if(vector[i] == number)
+      return 1;
+  return 0;
+}
+
+void printerVector(int vector[]){
+  for(int y = 0; y < 10; y++){
+    printf("%d\n",vector[y] );
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  int vector[10];
+  int number;
+  for(int k = 0; k < 10; k++){
+    do{
+      printf("Digite um número: ");
+      scanf("%d",&number);
+    }while(isExist(vector,number));
+
+    vector[k] = number;
+  }
+
+  printerVector(vector);
+
+  return 0;
+}
