6a545f8cd608e07ca1dac324de39e78cbb1c9782
Jéderson Gonçalves Martins <jedersongmoura@gmail.com>
2018-03-19 16:56:25 -0300

    Question 16

--- question16.c
+++ question16.c
@@ -1,6 +1,7 @@
 
 
 #include <stdio.h>
+#include <string.h>
 
 
 typedef struct{
@@ -17,9 +18,32 @@ float fahrenheitToCelsius(float tFahrenheit){
 }
 int main(int argc, char const *argv[]) {
   /* code */
-  Temperatura t[];
-  do{
+  Temperatura t[200];
+  char decision[3];
+  int pos = 0;
+  float value;
+  char type;
 
-  }while()
+  do{
+    printf("Informe a escala[Celsius(C) ou Fahrenheit(F):");
+    scanf("%s",&type);
+    printf("Informe o valor da Temperatura:");
+    scanf("%f",&value);
+
+    t[pos].value = value;
+    t[pos].type = type;
+    pos++;
+
+    printf("Digite \"fim\" para encerrar:");
+    scanf("%s",&decision);
+
+  }while(strcmp(decision,"fim") && strcmp(decision,"FIM")  && pos < 200);
+
+
+  while (t != NULL) {
+    printf("%s:%.2f -> %s:%.2f\n",t[0].type,t[0].value,(strcmp(t[0].type,"C") == 0 ? "F" : "C"),(strcmp(t[0].type,"C") == 0 ? celsiusToFahrenheit(t[0].value) : fahrenheitToCelsius(t[0].value)));
+  }
+
+  printf("fim");
   return 0;
 }
