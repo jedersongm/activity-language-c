0c7b2479f0c069ba4a86bb9cd1cda7fa8791b11b
Jéderson Gonçalves Martins <jedersongmoura@gmail.com>
2018-03-19 01:46:16 -0300

    Question 16: Conversão de temperatura

---
+++ question16.c
@@ -0,0 +1,25 @@
+
+
+#include <stdio.h>
+
+
+typedef struct{
+  float value;
+  char type;
+}Temperatura;
+
+float celsiusToFahrenheit(float tCelsius){
+  return (9*tCelsius/5)+32;
+}
+
+float fahrenheitToCelsius(float tFahrenheit){
+  return 5*(tFahrenheit-32)/9;
+}
+int main(int argc, char const *argv[]) {
+  /* code */
+  Temperatura t[];
+  do{
+
+  }while()
+  return 0;
+}
